7553e970f07b1df3e125e523c14e357eba48c868
Zeilar <philip_angelin@hotmail.com>
2025-07-25 16:18:24 +0200

    fix: always use http as ssl isn't supported on this strapi

--- api/handlers.ts
+++ api/handlers.ts
@@ -1,9 +1,6 @@
 export function strapiFetcher(input: `/${string}`, init?: RequestInit) {
-  return fetch(
-    `${process.env.NODE_ENV === "production" ? "https" : "http"}://strapi:1337${input}`,
-    {
-      ...init,
-      headers: { Authorization: `Bearer ${process.env.APP_STRAPI_API_KEY}`, ...init?.headers },
-    }
-  );
+  return fetch(`http://strapi:1337${input}`, {
+    ...init,
+    headers: { Authorization: `Bearer ${process.env.APP_STRAPI_API_KEY}`, ...init?.headers },
+  });
 }
